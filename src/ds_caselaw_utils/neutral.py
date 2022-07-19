10d69cbf5a3b70bc00fd344379e9b3d60a7863dc
David McKee <dragon@dxw.com>
2022-07-19 17:29:29 +0100

    Use poetry for test workflow

--- src/ds_caselaw_utils/neutral.py
+++ src/ds_caselaw_utils/neutral.py
@@ -2,16 +2,22 @@
 Convert neutral Citations to URL
 """
 
+import pathlib
 import re
 
 from ruamel.yaml import YAML
 
 yaml = YAML()
-with open("data/neutral_citation_regex.yaml") as f:
+datafile = pathlib.Path(__file__).parent / "data/neutral_citation_regex.yaml"
+with open(datafile) as f:
     citation_data = yaml.load(f)
 
 
 def neutral_url(citation):
+    """Given a neutral citation such as `[2020] EAT 17`,
+    return a public-API URL like `/eat/2020/17`, or None
+    if no match is found.
+    """
     for regex, groups in citation_data:
         if match := re.match(regex, citation):
             url_components = "/".join([match.groups()[x - 1] for x in groups])
