0655f3158218b0310664bc737a88117136ccb163
David McKee <dragon@dxw.com>
2022-07-19 16:38:55 +0100

    Rewrite to use YAML file for configuration

--- src/ds_caselaw_utils/neutral.py
+++ src/ds_caselaw_utils/neutral.py
@@ -4,26 +4,15 @@ Convert neutral Citations to URL
 
 import re
 
-# Reading the match_data:
-# the components of the URL for [2022] EAT 1 are the
-# 2nd, 1st and 3rd components of the neutral citation,
-# so the URL becomes eat/2022/1
+from ruamel.yaml import YAML
 
-match_data = {
-    # fmt: off
-    r"^\[(\d{4})\] (UKSC|UKPC) (\d+)$": [2, 1, 3],
-    r"^\[(\d{4})\] (EWCA) (Civ|Crim) (\d+)$": [2, 3, 1, 4],
-    r"^\[(\d{4})\] (EWHC) (\d+) \((Admin|Admlty|Ch|Comm|Costs|Fam|IPEC|Pat|QB|SCCO|TCC)\)$": [2, 4, 1, 3],  # noqa: E501
-    r"^\[(\d{4})\] (EWFC|EWCOP) (\d+)$": [2, 1, 3],
-    r"^\[(\d{4})\] (UKUT) (\d+) \((AAC|IAC|LC|TCC)\)$": [2, 4, 1, 3],
-    r"^\[(\d{4})\] (EAT) (\d+)$": [2, 1, 3],
-    r"^\[(\d{4})\] (UKFTT) (\d+) \((TC|GRC)\)$": [2, 4, 1, 3],
-    # fmt: on
-}
+yaml = YAML()
+with open("data/neutral_citation_regex.yaml") as f:
+    citation_data = yaml.load(f)
 
 
 def neutral_url(citation):
-    for regex, groups in match_data.items():
+    for regex, groups in citation_data:
         if match := re.match(regex, citation):
             url_components = "/".join([match.groups()[x - 1] for x in groups])
             return f"/{url_components}".lower()
